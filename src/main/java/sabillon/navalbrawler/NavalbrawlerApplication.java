f8bbbc72fe0aa91a22ea3df88e53994327fba102
Miguel Sabillon <mikesabillon2014@gmail.com>
2019-08-31 21:54:17 +0200

    first commit of base project

---
+++ src/main/java/sabillon/navalbrawler/NavalbrawlerApplication.java
@@ -0,0 +1,13 @@
+package sabillon.navalbrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavalbrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavalbrawlerApplication.class, args);
+	}
+
+}
